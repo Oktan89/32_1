73520c626f4a81ec4ad46938dd29face473d7079
-=Oktan=- <r.zhigalenko@mail.ru>
2021-10-27 23:52:37 +0500

    completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <fstream>
 #include "nlohmann/json.hpp"
 
@@ -11,6 +12,37 @@ int main()
     file >> film;
     file.close();
 
+    std::string name;
+    std::cout << "Enter actors name: ";
+    std::getline(std::cin, name);
+    
+    std::vector<std::string> f;
+
     for(auto it = film.begin(); it != film.end(); ++it)
-        std::cout << it.key() << " : " << it.value() << std::endl;
+    {
+        auto act = it->find("actors");
+
+        if(act != it->end())
+        {
+            auto na = act->find(name);
+            if(na != act->end())
+            {
+               std::string out = "Film: " +  it.key()
+                    + ", role in the film : " + nlohmann::to_string(na.value());
+               f.push_back(out);
+            }
+               
+        }
+    }
+    
+    if(f.size() > 0)
+    {
+        for(auto &x : f)
+        std::cout<< x <<std::endl;
+    }
+    else
+    {
+        std::cout << "Sorry, no such actor has been found";
+    }
+    
 }
